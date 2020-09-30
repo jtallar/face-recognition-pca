0be67069959d03b0646ddce55284c0db3bc11544
Matias Brula <mbrula@itba.edu.ar>
2020-09-30 16:17:41 -0300

    something there

--- test.py
+++ test.py
@@ -43,11 +43,9 @@ dir = './data'
 
 # recognize the ohm image
 ohm_img = l.get_ohm_image(faces[0], dir)
-print("Ohm img, ", ohm_img)
 
 # searches for the index of the matching face
 i = l.face_space_distance(ohm_img, dir)
-print(i)
 
 # gets the corresponding path given the index
 path = l.get_matching_path(i, dir)
