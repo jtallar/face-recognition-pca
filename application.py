6f6aaac5101ef595ace34a6771e9e166a6b50ef2
Matias Brula <mbrula@itba.edu.ar>
2020-10-05 15:58:21 -0300

    add some magic on app2

--- application.py
+++ application.py
@@ -8,8 +8,6 @@ import library as l
 import os
 import argparse
 
-# DATABASE_PATH = "/Users/geronimomaspero/Desktop/mna-tpe1/data"
-# INITIAL_DIR = "/Users/geronimomaspero/Desktop/mna-tpe1"
 
 # Read arguments
 ap = argparse.ArgumentParser()
@@ -20,72 +18,6 @@ ap.add_argument("-c", "--confidence", type=float, default=0.2, help="confidence 
 args = vars(ap.parse_args())
 
 
-#db = Database('store.db')
-
-
-
-
-#---------------------# FUNCTIONS #---------------------#
-
-# def populate_list():
-#     parts_list.delete(0, END)
-#     for row in db.fetch():
-#         parts_list.insert(END, row)
-
-
-# def add_item():
-#     if part_text.get() == '' or customer_text.get() == '' or retailer_text.get() == '' or price_text.get() == '':
-#         messagebox.showerror('Required Fields', 'Please include all fields')
-#         return
-#     db.insert(part_text.get(), customer_text.get(),
-#               retailer_text.get(), price_text.get())
-#     parts_list.delete(0, END)
-#     parts_list.insert(END, (part_text.get(), customer_text.get(),
-#                             retailer_text.get(), price_text.get()))
-#     clear_text()
-#     populate_list()
-
-
-# def select_item(event):
-#     try:
-#         global selected_item
-#         index = parts_list.curselection()[0]
-#         selected_item = parts_list.get(index)
-
-#         part_entry.delete(0, END)
-#         part_entry.insert(END, selected_item[1])
-#         customer_entry.delete(0, END)
-#         customer_entry.insert(END, selected_item[2])
-#         retailer_entry.delete(0, END)
-#         retailer_entry.insert(END, selected_item[3])
-#         price_entry.delete(0, END)
-#         price_entry.insert(END, selected_item[4])
-#     except IndexError:
-#         pass
-
-
-# def remove_item():
-#     db.remove(selected_item[0])
-#     clear_text()
-#     populate_list()
-
-
-# def update_item():
-#     db.update(selected_item[0], part_text.get(), customer_text.get(),
-#               retailer_text.get(), price_text.get())
-#     populate_list()
-
-
-# def clear_text():
-#     part_entry.delete(0, END)
-#     customer_entry.delete(0, END)
-#     retailer_entry.delete(0, END)
-#     price_entry.delete(0, END)
-
-
-#
-# -------------------------------------- CODIGO PROBETA -----------------------------------------------------------------------------------
-#
 def popupresults(face):
     #Build windows
     popup2 = tk.Toplevel(app)
@@ -119,7 +51,6 @@ def popupresults(face):
     #when this popup is done waiting for user's input, it will return
     popup2.wait_window()
 
-
         
 def open_image():
     global my_image
@@ -138,11 +69,7 @@ def open_image():
         for face in faces:
             l.show_face(face)
             #Popeamos una ventanita que le pida nombre para la imagen y la meta en la scroll list.
-            name = popupresults(face)
-
-    
-
-        
+            name = popupresults(face)      
 
         
 def popupmessage(face):
@@ -236,64 +163,6 @@ upload_image_label2.place(height=50, width=400, x=400, y=70)
 # Upload Image Button - for RECOGNIZE FACE section
 upload_image_button2 = Button(app, text="Open Image...", command=open_image)
 upload_image_button2.place(height=50, width=400, x=400, y=140)
-
-
-
-
-
-
-
-
-
-
-
-
-# # Retailer
-# retailer_text = StringVar()
-# retailer_label = Label(app, text='Retailer', font=('bold', 14))
-# retailer_label.grid(row=1, column=0, sticky=W)
-# retailer_entry = Entry(app, textvariable=retailer_text)
-# retailer_entry.grid(row=1, column=1)
-
-# # Price
-# price_text = StringVar()
-# price_label = Label(app, text='Price', font=('bold', 14))
-# price_label.grid(row=1, column=2, sticky=W)
-# price_entry = Entry(app, textvariable=price_text)
-# price_entry.grid(row=1, column=3)
-
-#SCROLL
-# # Parts List (Listbox)
-# parts_list = Listbox(app, height=16, width=50)
-# parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
-
-# # Create scrollbar
-# scrollbar = Scrollbar(app)
-# scrollbar.grid(row=3, column=3)
-
-# # Set scroll to listbox
-# parts_list.configure(yscrollcommand=scrollbar.set)
-# scrollbar.configure(command=parts_list.yview)
-
-# # Bind select
-# parts_list.bind('<<ListboxSelect>>', select_item)
-
-
-
-# Buttons
-# add_btn = Button(app, text='Add Part', width=12, command=add_item)
-# add_btn.grid(row=2, column=0, pady=20)
-
-# remove_btn = Button(app, text='Remove Part', width=12, command=remove_item)
-# remove_btn.grid(row=2, column=1)
-
-# update_btn = Button(app, text='Update Part', width=12, command=update_item)
-# update_btn.grid(row=2, column=2)
-
-# clear_btn = Button(app, text='Clear Input', width=12, command=clear_text)
-# clear_btn.grid(row=2, column=3)
-
-
 
 app.title('Face Recognition - Probeta Technologies')
 #app['background']='#DBF3FA'
