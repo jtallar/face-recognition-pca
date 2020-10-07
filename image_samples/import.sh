f21a1d3396de6b176c886046a383496a266b48d6
Julian Tallar <jtallar@itba.edu.ar>
2020-10-07 00:45:14 -0300

    Test data

--- image_samples/import.sh
+++ image_samples/import.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-j=11
-mkdir person$j
-mkdir person$j-test
-	for i in {110..117}
+for j in {12..25}
+do
+	a=$(expr $j \* 10)
+	b=$(expr $a + 7)
+	c=$(expr $b + 1)
+	d=$(expr $c + 1)
+	for ((i=$a; i<=$b; i++))
 	do
 		mv ~/Downloads/Olivetti-PNG-master/images/image-$i.png person$j
 	done
-	for i in {118..119}
-        do
-                mv ~/Downloads/Olivetti-PNG-master/images/image-$i.png person$j-test
-        done
+	for ((i=$c; i<=$d; i++))
+	do
+		mv ~/Downloads/Olivetti-PNG-master/images/image-$i.png person$j-test
+	done
+done
